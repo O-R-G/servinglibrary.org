3679e30c2e313c51e1970fd6da4924ffff4bc861
david reinfurt <reinfurt@ussava.pair.com>
2016-11-25 11:53:51 -0500

    add views/object.php *dev*

--- index.php
+++ index.php
@@ -13,6 +13,10 @@ if($uri[1])
 		$view.= "read.php";
 	else if($uri[1] == "collect")
 		$view.= "collect.php";
+	else if($uri[1] == "objects" && !$uri[2])
+		$view.= "objects.php";
+	else if($uri[1] == "objects" && $uri[2])
+		$view.= "words.php";
 	else if($uri[1] == "join") {
         	$showsubscribe = true;
 		$view.="words.php";
