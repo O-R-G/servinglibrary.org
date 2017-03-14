2d14e5d01fc85d5b8f940f41372f62267caf080b
david reinfurt <reinfurt@ussava.pair.com>
2017-03-14 13:15:55 -0400

    update collection.php ignore objects starting with .

--- views/collection.php
+++ views/collection.php
@@ -32,16 +32,18 @@ foreach($objects as $o)
 	$o_url = $uu->url."/".$o['url'];
 	
 	// display object and caption
-	?><div class="thumbsContainer object">
-		<a href="<? echo $o_url; ?>">
-			<div class="coverContainer"><?
-				?><img src="<? echo $m_url; ?>"><?
+	if (substr($o['name1'], 0, 1) != ".") {
+		?><div class="thumbsContainer object">
+			<a href="<? echo $o_url; ?>">
+				<div class="coverContainer"><?
+					?><img src="<? echo $m_url; ?>"><?
+				?></div>
+			</a>
+			<div class="captionContainer caption"><? 
+				echo $caption; 
 			?></div>
-		</a>
-		<div class="captionContainer caption"><? 
-			echo $caption; 
-		?></div>
-	</div><?
+		</div><?
+	}
 }
 	
 ?><div id="Menu" class="TSLContainer body">
