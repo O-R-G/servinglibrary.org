5a38419ca0a5063b38d691f8d5f5af932719b742
david reinfurt <reinfurt@Adriano-Olivetti.local>
2017-03-27 16:13:45 +0200

    ibid

--- views/foot.php
+++ views/foot.php
@@ -3,7 +3,7 @@
             // menu
             echo ($uu->url != 'introduction') ? "<a href='introduction'>Introduction</a> — " : "Introduction — ";
             echo ($uu->url != 'publication') ? "<a href='publication'>Publication</a> / " : "Publication / ";
-            echo ($uu->url != 'program') ? "<a href='program'>Program</a> / " : "Program / ";
+            echo ($uu->url != 'programs') ? "<a href='programs'>Programs</a> / " : "Programs / ";
             echo ($uu->url != 'collection-new') ? "<a href='collection-new'>Collection</a>" : "Collection";
         ?>
 </div>
