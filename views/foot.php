6f5168ed995ca705a4e07361c145a68a2a84fe96
david reinfurt <reinfurt@Adriano-Olivetti.local>
2017-03-27 18:28:25 +0200

    fix menubar for second levels

--- views/foot.php
+++ views/foot.php
@@ -1,10 +1,10 @@
 		</div>
         <div id="Menu" class="TSLContainer body"><?
             // menu
-            echo ($uu->url != 'introduction') ? "<a href='introduction'>Introduction</a> — " : "Introduction — ";
-            echo ($uu->url != 'publication') ? "<a href='publication'>Publication</a> / " : "Publication / ";
-            echo ($uu->url != 'programs') ? "<a href='programs'>Programs</a> / " : "Programs / ";
-            echo ($uu->url != 'collection-new') ? "<a href='collection-new'>Collection</a>" : "Collection";
+            echo ($uu->url != 'introduction') ? "<a href='/introduction'>Introduction</a> — " : "Introduction — ";
+            echo ($uu->url != 'publication') ? "<a href='/publication'>Publication</a> / " : "Publication / ";
+            echo ($uu->url != 'programs') ? "<a href='/programs'>Programs</a> / " : "Programs / ";
+            echo ($uu->url != 'collection-new') ? "<a href='/collection-new'>Collection</a>" : "Collection";
         ?>
 </div>
 
