126ee6a9f630fb652ed5581eb00762a222ebbb68
david reinfurt <reinfurt@ussava.pair.com>
2017-10-29 10:18:09 -0400

    add donate button on downloads page

--- views/read.php
+++ views/read.php
@@ -108,7 +108,17 @@ if(count($media_all) > 2)
 			};
 		</script><?
 		}
-	?></div>
+	?>
+	<br />
+	<span class='green-hi'>&nbsp;Please consider donating $1 per download. *Thank you.*&nbsp;</span>
+	<br /><br />
+	<form name="Donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="hosted_button_id" value="H4B76UC2KFXCE">
+		<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+		<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+	</form>
+	</div>
 	<form id="download-form" action="/lib/download.php" method="post">
 		<input type="hidden" name="source_name" value="<? echo $source_name; ?>">
 		<input type="hidden" name="display_name" value="<? echo $display_name; ?>">
