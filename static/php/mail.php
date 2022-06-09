5bcca03e8e42e01a94c951217a9a0265e0322309
david reinfurt <reinfurt@o-r-g.com>
2022-06-08 20:43:29 -0400

    add mailgun

---
+++ static/php/mail.php
@@ -0,0 +1,25 @@
+<?
+    /*
+        mailgun php sdk
+        https://github.com/mailgun/mailgun-php
+    */
+
+    require 'vendor/autoload.php';
+    use Mailgun\Mailgun;
+
+    function mailgun($domain, $msg) {
+        $mailgun_client_id = getenv('MAILGUN_CLIENT_ID');
+        try {
+            $mg = Mailgun::create($mailgun_client_id);
+            $mg->messages()->send($domain, [
+    	        
+                'from'	=> $msg['from'],
+	            'to'	=> $msg['to'],
+	            'bcc'	=> $msg['bcc'],
+    	        'subject' => $msg['subject'],
+	            'text'	=> $msg['text']
+            ]);
+        } catch (Exception $e) {
+        }
+    }
+?>
