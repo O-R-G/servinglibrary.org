e4125087146def45d32aeca318d5ecfae61f39d1
wei <wei@o-r-g.com>
2025-02-25 19:49:46 +0800

    add hash to shop.js

--- views/shop.php
+++ views/shop.php
@@ -32,7 +32,7 @@ $isSandbox = isset($_GET['isSandbox']);
 <script>
     var isSandbox = <?= json_encode($isSandbox); ?>;
 </script>
-<script src="/static/js/shop.js"></script>
+<script src="/static/js/shop.js#20250225"></script>
 <script src="/static/js/cookie.js"></script><?
 
     /*currency
