de8e7025b01886aa895e474fb7bdecd54ecc69e5
wei <wei@o-r-g.com>
2026-06-23 10:49:31 +0800

    update thx.php

--- views/thx.php
+++ views/thx.php
@@ -15,14 +15,14 @@
         $domain = 'mail.servinglibrary.org';
         $msg['from'] = 'shop@servinglibrary.org';
         $msg['to'] = $email;
-        $msg['bcc'] = $_GET['currency'] == 'USD' ? $debug_email : 'stuart@servinglibrary.org';
+        $msg['bcc'] = isset($_GET['currency']) && strtolower($_GET['currency']) == 'usd' ? $debug_email : 'stuart@servinglibrary.org';
         $msg['subject'] = 'The Serving Library says thanks for your order';
         $msg['text'] = "*\n\nThank you for purchasing the following item(s):\n";
         foreach($items as $it)
             $msg['text'] .= "\n" . $it;
 
         $msg['text'] .= "\n\nWe appreciate the order -- this really keeps our publishing engine running. Shipping takes approximately five days from ";
-        $msg['text'] .= $_GET['currency'] == 'USD' ? 'US.' : 'EU.';
+        $msg['text'] .= isset($_GET['currency']) && strtolower($_GET['currency']) == 'usd' ? 'US.' : 'EU.';
         $msg['text'] .= "\n\nAny questions or concerns, please email info@servinglibrary.org.";
         $msg['text'] .= "\n\n*\n\nEnjoy, tell your friends, and so forth.\n\n*\n\nhttps://www.servinglibrary.org";
 
